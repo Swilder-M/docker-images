af2a59d63e567535ffe624e2a3cecf2d6483591b
Swilder-M <i@codming.com>
2024-12-08 09:45:27 +0800

    Initial commit
    Signed-off-by: Swilder-M <i@codming.com>

--- run-openconnect.sh
+++ run-openconnect.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-# snell-server -c /etc/snell-server.conf >> /var/log/snell.log 2>&1 &
+/usr/bin/xray -c /etc/xray.json &
 
-# if [ $? -ne 0 ]; then
-#     echo "Failed to start Snell service."
-#     exit 1
-# fi
-
-# 使用传入的环境变量启动 OpenConnect
 openconnect -v \
   --timestamp \
   --force-dpd=10 \
